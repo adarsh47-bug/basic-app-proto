6a692ad86fefc3c86e541d468be7ad3a4003ef19
adarsh47-bug <adarshkadam5977@gmail.com>
2024-09-19 15:58:28 +0530

    posts loaded

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db'); // Import the MongoDB connection function
+const postRoutes = require('./routes/postRoutes'); // Post-related routes
+
+
+// Initialize MongoDB connection
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,6 +19,12 @@ app.get('/', (req, res) => {
 app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello from the Adarsh Kadam!' });
 });
+
+app.use('/api/posts', postRoutes);
+// app.use('/api/events', eventRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
